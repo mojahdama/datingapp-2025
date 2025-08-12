b22f1e78ed7628cc598b8f16e241ef01df950c3d
Mojahd Amayreh <mojahdama@gmail.com>
2025-08-12 09:58:18 +0300

    Initial commit

---
+++ API/Controllers/MembersController.cs
@@ -0,0 +1,30 @@
+using System.Net.WebSockets;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MembersController(AppDbContext context) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
+        {
+            var members =await context.Users.ToListAsync();
+            return members;
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetMembers(string id)
+        {
+            var members = await context.Users.FindAsync(id);
+            if (members == null) return NotFound();
+
+            return members;
+        }
+
+    }
+}
