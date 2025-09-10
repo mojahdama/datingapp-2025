431249e2f9bea4c59bf8ae89368efbe030ea382d
Mojahd Amayreh <mojahdama@gmail.com>
2025-09-10 13:16:28 +0300

    End of section 7

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -6,7 +6,7 @@ namespace API.DTOs;
 public class RegisterDto
 {
     [Required]
-    public string DisplayName { get; set; } = ";";
+    public string DisplayName { get; set; } = "";
     [Required]
     [EmailAddress]
     public string Email { get; set; } = "";
---
+++ API/Errors/ApiException.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Errors
+{
+
+    public class ApiException (int statusCode , string message , string? details)
+    {
+        public int StatusCode { get; set; } = statusCode;
+        public string Message { get; set; } = message;
+        public string? Details { get; set; } = details;
+    }
+}
