f34c686a1136730877a5ad2da5e8a1cf6b93c692
Mojahd Amayreh <mojahdama@gmail.com>
2025-10-19 15:31:55 +0300

    End of secction 11

--- client/src/core/services/member-service.ts
+++ client/src/core/services/member-service.ts
@@ -1,4 +1,4 @@
-import { HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { EditableMember, Member, Photo } from '../../types/member';
@@ -8,47 +8,62 @@ import { tap } from 'rxjs';
   providedIn: 'root'
 })
 export class MemberService {
- private http  = inject(HttpClient);
- private baseUrl = environment.apiUrl;
- editMode = signal(false);
+  private http = inject(HttpClient);
+  private baseUrl = environment.apiUrl;
+  editMode = signal(false);
   member = signal<Member | null>(null);
 
- getMembers(){
-  return this.http.get<Member[]>(this.baseUrl + 'members')
- }
+  getMembers() {
+    return this.http.get<Member[]>(this.baseUrl + 'members')
+  }
 
- getMember(id:string) {
+  getMember(id: string) {
 
-  return this.http.get<Member>(this.baseUrl +'members/' + id).pipe(
-    tap(member => {
-      this.member.set(member);
-    })
-  )
- }
+    return this.http.get<Member>(this.baseUrl + 'members/' + id).pipe(
+      tap(member => {
+        this.member.set(member);
+      })
+    )
+  }
 
- getMemberPhotos(id:string) {
+  getMemberPhotos(id: string) {
 
-  return this.http.get<Photo[]>(this.baseUrl +'members/' + id + '/photos');
- }
+    return this.http.get<Photo[]>(this.baseUrl + 'members/' + id + '/photos');
+  }
 
- updateMember(member : EditableMember) {
-  return this.http.put(this.baseUrl +'members',member);
- }
+  updateMember(member: EditableMember) {
+    return this.http.put(this.baseUrl + 'members', member);
+  }
 
-//  getMember(id:string) {
+  uploadePhoto(file: File) {
+    const formData = new FormData();
+    formData.append('file', file);
+    return this.http.post<Photo>(this.baseUrl + 'members/add-photo', formData);
+  }
 
-//   return this.http.get<Member>(this.baseUrl +'members/' + id) , this.getHttpOptipons();
-//  }
+  setMainPhoto(photo: Photo) {
+    return this.http.put(this.baseUrl + 'members/set-main-photo/' + photo.id, {});
+  }
 
-//  private getHttpOptipons(){
-// return {
-//  headers : new HttpHeaders ({
-//   Authorization : 'Bearer ' + this.accountService.currentUser()?.token
-//  })
+  deletePhoto(photoId: number) {
+    return this.http.delete(this.baseUrl + 'members/delete-photo/' + photoId);
+  }
 
-// }
 
-//  }
+  //  getMember(id:string) {
+
+  //   return this.http.get<Member>(this.baseUrl +'members/' + id) , this.getHttpOptipons();
+  //  }
+
+  //  private getHttpOptipons(){
+  // return {
+  //  headers : new HttpHeaders ({
+  //   Authorization : 'Bearer ' + this.accountService.currentUser()?.token
+  //  })
+
+  // }
+
+  //  }
 
 
 }
--- client/src/features/members/member-photos/member-photos.ts
+++ client/src/features/members/member-photos/member-photos.ts
@@ -1,26 +1,72 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { MemberService } from '../../../core/services/member-service';
 import { ActivatedRoute } from '@angular/router';
-import { Photo } from '../../../types/member';
-import { Observable } from 'rxjs';
-import { AsyncPipe } from '@angular/common';
+import { Member, Photo } from '../../../types/member';
+import { ImageUpload } from "../../../shared/image-upload/image-upload";
+import { AccountService } from '../../../core/services/account-service';
+import { User } from '../../../types/user';
+import { StarButton } from "../../../shared/star-button/star-button";
+import { DeleteButton } from "../../../shared/delete-button/delete-button";
 
 @Component({
   selector: 'app-member-photos',
-  imports: [AsyncPipe],
+  imports: [ImageUpload, StarButton, DeleteButton],
   templateUrl: './member-photos.html',
   styleUrl: './member-photos.css'
 })
-export class MemberPhotos {
+export class MemberPhotos implements OnInit {
 
-  private memberService = inject(MemberService);
+  protected memberService = inject(MemberService);
+  protected accountService = inject(AccountService)
   private route = inject(ActivatedRoute);
-  protected photos$? : Observable<Photo[]>;
+  protected photos = signal<Photo[]>([]);
+  protected loading = signal(false);
 
-  constructor (){
+  ngOnInit(): void {
     const memberId = this.route.parent?.snapshot.paramMap.get('id');
-    if(memberId){
-      this.photos$ = this.memberService.getMemberPhotos(memberId);
+    if (memberId) {
+      this.memberService.getMemberPhotos(memberId).subscribe({
+        next: photos => this.photos.set(photos)
+      })
     }
   }
+
+  onUploadImage(file: File) {
+    this.loading.set(true);
+    this.memberService.uploadePhoto(file).subscribe({
+      next :photo =>{
+          this.memberService.editMode.set(false);
+          this.loading.set(false);
+          this.photos.update(photos => [...photos , photo])
+      },
+      error : error =>{
+        console.log('Error uploading image : ' , error);
+        this.loading.set(false);
+      }
+    })
+  }
+
+  setMainPhoto(photo : Photo){
+    this.memberService.setMainPhoto(photo).subscribe({
+      next:() =>{
+        const currentUser = this.accountService.currentUser();
+        if (currentUser) currentUser.imageUrl = photo.url;
+        this.accountService.setCurrentUser(currentUser as User);
+        this.memberService.member.update(member =>({
+          ...member,
+          imageUrl:photo.url
+        })as Member)
+      }
+    })
+  }
+
+  deletePhoto(photoId : number){
+    this.memberService.deletePhoto(photoId).subscribe({
+      next : () => {
+        this.photos.update(photos => photos.filter(x => x.id !== photoId))
+      }
+    })
+  }
+
+
 }
